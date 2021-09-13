94c5cf913da9d842d3b8be76920519bf53ad0b1e
Gustavo <gustavcampos73@gmail.com>
2021-09-13 01:32:32 -0300

    input validation

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="text-center">
       <h1 class="text-3xl">Busque por um CEP</h1>
       <form @submit.prevent="cepSearch(text)">
-        <input type="text" v-model="text" />
+        <input pattern="[0-9]+" title="Porfavor, insira um CEP válido" required="required" type="text" v-model="cep" />
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
           Button
         </button>
@@ -21,13 +21,13 @@ export default {
   components: { Results },
   data() {
     return {
-      text: '',
+      cep: '',
       results: null,
     };
   },
   methods: {
-    async cepSearch(text) {
-      const res = await fetch('https://cep.awesomeapi.com.br/json' + text);
+    async cepSearch(cep) {
+      const res = await fetch('https://cep.awesomeapi.com.br/json' + cep);
       const data = await res.json();
       this.results = data;
       console.log(this.results);
