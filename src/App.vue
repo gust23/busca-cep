bf61e8ddac466d3462ce6b946647d0083f3cdc84
Gustavo <gustavcampos73@gmail.com>
2021-09-13 01:09:46 -0300

    view done, missing styles

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
-  <div class="container mx-auto">
-    <h1 class="text-red-700">bubububu</h1>
+  <div class="flex h-screen justify-center items-center bg-green-50">
+    <div class="text-center">
+      <h1 class="text-3xl">Busque por um CEP</h1>
+      <form @submit.prevent="cepSearch(text)">
+        <input type="text" v-model="text" />
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          Button
+        </button>
+      </form>
+
+      <Results v-if="results" :results="results" />
+    </div>
   </div>
 </template>
 
 <script>
+import Results from './components/Results.vue';
 export default {
   name: 'App',
-  components: {},
+  components: { Results },
+  data() {
+    return {
+      text: '',
+      results: null,
+    };
+  },
+  methods: {
+    async cepSearch(text) {
+      const res = await fetch('https://cep.awesomeapi.com.br/json' + text);
+      const data = await res.json();
+      this.results = data;
+      console.log(this.results);
+    },
+  },
 };
 </script>
 
---
+++ src/components/Results.vue
@@ -0,0 +1,17 @@
+<template lang="">
+  <div>
+    <div>
+      <h2>Resutado da busca:</h2>
+      <p>Logradouro: {{ results.address }}</p>
+      <p>Bairro: {{ results.district }}</p>
+      <p>Localização: {{ results.city }} {{ results.state }}</p>
+      <p>Código de Área: {{ results.ddd }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['results'],
+};
+</script>
+<style lang=""></style>
