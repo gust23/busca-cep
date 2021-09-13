0cf0501bca3766fcc05e881eea59792c73dcd438
Gustavo <gustavcampos73@gmail.com>
2021-09-13 20:13:11 -0300

    fetch error notification and some style

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <div class="flex h-screen justify-center items-center bg-green-50">
-    <div class="text-center">
-      <h1 class="text-3xl">Busque por um CEP</h1>
-      <form @submit.prevent="cepSearch(text)">
-        <input pattern="[0-9]+" title="Porfavor, insira um CEP válido" required="required" type="text" v-model="cep" />
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-          Button
-        </button>
+  <div class="h-screen w-screen flex flex-col justify-center items-center bg-green-50">
+    <div class="bg-green-300 border-green-600 border-b p-6 m-4 rounded">
+      <h1 class="text-center w-full font-bold text-3xl text-gray-600 p-4">Busque por um CEP</h1>
+      <div class="w-full bg-gray-400 my-3" style="height: 1px"></div>
+      <form @submit.prevent="cepSearch(cep)">
+        <div class="flex flex-col gap-4 px-0 py-4">
+          <input
+            class="text-center self-center py-2  border border-gray-200 w-min"
+            pattern="[0-9]+"
+            title="Por favor, insira um CEP válido"
+            required="required"
+            type="text"
+            placeholder="Digite o CEP desejado"
+            v-model="cep"
+          />
+          <button
+            class=" m-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded w-min self-center"
+          >
+            Buscar
+          </button>
+        </div>
       </form>
-
-      <Results v-if="results" :results="results" />
+      <div class=" w-max ">
+        <Results v-if="results && !errorMessage" :results="results" />
+        <p v-if="errorMessage">{{ errorMessage }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -23,14 +38,25 @@ export default {
     return {
       cep: '',
       results: null,
+      errorMessage: '',
     };
   },
   methods: {
     async cepSearch(cep) {
-      const res = await fetch('https://cep.awesomeapi.com.br/json' + cep);
+      const res = await fetch('https://cep.awesomeapi.com.br/json/' + cep);
       const data = await res.json();
-      this.results = data;
-      console.log(this.results);
+
+      if (data.status) {
+        if (data.status === 404) {
+          this.errorMessage = data.message;
+        }
+        if (data.status === 400) {
+          this.errorMessage = data.message;
+        }
+      } else {
+        this.results = data;
+        this.errorMessage = '';
+      }
     },
   },
 };
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,10 +1,10 @@
 <template lang="">
   <div>
     <div>
-      <h2>Resutado da busca:</h2>
+      <h2 class="text-center font-bold">Resutado da busca:</h2>
       <p>Logradouro: {{ results.address }}</p>
       <p>Bairro: {{ results.district }}</p>
-      <p>Localização: {{ results.city }} {{ results.state }}</p>
+      <p>Localização: {{ results.city }} - {{ results.state }}</p>
       <p>Código de Área: {{ results.ddd }}</p>
     </div>
   </div>
